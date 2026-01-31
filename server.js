497746e2819ddc64a82c067cfe70c0a4f45c4262
thesaud <saud.person@gmail.com>
2026-01-31 17:11:19 +0300

    todo-api

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const TaskSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean,
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
